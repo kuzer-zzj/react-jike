c4ae3cae9bcaa63249ca9234053bad05bfc9fa60
zzj <156321781@qq.com>
2023-12-06 10:42:40 +0800

    菜单点击跳转

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -6,29 +6,34 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
 const items = [
   {
     label: '首页',
-    key: '1',
+    key: '/',
     icon: <HomeOutlined />,
   },
   {
     label: '文章管理',
-    key: '2',
+    key: '/article',
     icon: <DiffOutlined />,
   },
   {
     label: '创建文章',
-    key: '3',
+    key: '/publish',
     icon: <EditOutlined />,
   },
 ]
 
 const GeekLayout = () => {
+const navigate= useNavigate()
+  const menuClick =(route)=>{
+    console.log('菜单点击：', route.key)
+    navigate(route.key)
+  }
   return (
     <Layout>
       <Header className="header">
@@ -45,6 +50,7 @@ const GeekLayout = () => {
       <Layout>
         <Sider width={200} className="site-layout-background">
           <Menu
+          onClick={menuClick}
             mode="inline"
             theme="dark"
             defaultSelectedKeys={['1']}
