90675bd104bcaa62ea8a0c948a7fdf29a24318e9
zzj <156321781@qq.com>
2023-12-06 10:33:52 +0800

    二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,35 @@
-import LayOut from "@/pages/layout";
+import GeekLayout from "@/pages/layout";
 import Login from "@/pages/login";
 import AuthRoute from "@/components/AuthRoute";
 
 import { createBrowserRouter } from "react-router-dom";
+import Home from "@/pages/home";
+import Article from "@/pages/article";
+import Publish from "@/pages/publish";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <AuthRoute>
-        <LayOut></LayOut>
+        <GeekLayout></GeekLayout>
       </AuthRoute>
     ),
+    children: [
+      {
+        // path: "/home",
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/article",
+        element: <Article></Article>,
+      },
+      {
+        path: "/publish",
+        element: <Publish></Publish>,
+      },
+    ],
   },
   {
     path: "/login",
